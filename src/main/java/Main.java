5d0cb4a5cbfd96bc9c76031f01b085cdc917dc29
Rick Kase <resak@purdue.edu>
2020-05-23 12:03:56 -0400

    Created the basic architecture of this project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import py4j.GatewayServer;
 public class Main {
 
 	public static void main(String[] args) {
-
+		GatewayServer gatewayServer = new GatewayServer(new StackEntryPoint());
+		gatewayServer.start();
+		System.out.println("Gateway Server Started");
 	}
 }
--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -1,2 +1,22 @@
 public class Manager {
+
+
+	/** empty constructor */
+	public Manager() { }
+
+	/**
+	 * Creates a new stack and returns it
+	 * @return MyStack object
+	 */
+	public MyStack getStack() {
+		return new MyStack();
+	}
+
+	/**
+	 * Creates a new Queue and returns it
+	 * @return MyQueue object
+	 */
+	public MyQueue getQueue() {
+		return new MyQueue();
+	}
 }
